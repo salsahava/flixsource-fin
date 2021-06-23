cd5461a1ed589931da94720dc6e3b80f06308895
salsahava <1shavaqabita@gmail.com>
2021-06-23 16:19:39 +0700

    Fixed splashscreen

--- app/src/main/java/com/dicoding/salsahava/flixsource/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/dicoding/salsahava/flixsource/ui/SplashScreenActivity.kt
@@ -1,9 +1,11 @@
 package com.dicoding.salsahava.flixsource.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.salsahava.flixsource.databinding.ActivitySplashScreenBinding
 import com.dicoding.salsahava.flixsource.ui.home.HomeActivity
@@ -16,7 +18,15 @@ class SplashScreenActivity : AppCompatActivity() {
         val splashScreenActivityBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(splashScreenActivityBinding.root)
 
-        window.insetsController?.hide(WindowInsets.Type.statusBars())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) window.insetsController?.hide(
+            WindowInsets.Type.statusBars()
+        )
+        else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         Handler(mainLooper).postDelayed({
             val intent = Intent(this@SplashScreenActivity, HomeActivity::class.java)
